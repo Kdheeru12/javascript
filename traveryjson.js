3798065e5f91e01b3e1fefe414a1621fab2ed3d0
Kdheeru12 <60852451+Kdheeru12@users.noreply.github.com>
2020-11-05 18:32:35 +0530

    Update traveryjson.js

--- traveryjson.js
+++ traveryjson.js
@@ -1,4 +1,32 @@
 const search = document.querySelector('input')
 const match = document.querySelector('.match')
 
-search.addEventListener('keyup',searchResults)
+search.addEventListener('input',() =>searchStates(search.value))
+
+const jso = () =>{
+     fetch('data.json')
+    .then((i) => i.json())
+    .then((j) => console.log(j) )
+} 
+
+ 
+const searchStates = async searchText =>{
+    const i = await fetch('data.json')
+    const states = await i.json()
+    
+    let matches = states.filter( (j) =>{
+        const regex = new RegExp(`^${searchText}`,'gi')
+        return j.name.match(regex) || j.abbr.match(regex)
+    })
+    if( searchText.length === 0){
+        matches = []
+    }
+    let output = ''
+    const matchhtml = matches.forEach((match) =>{
+    output += `
+                ${match.name}
+                <br>
+            `
+    })
+    match.innerHTML = output
+}
